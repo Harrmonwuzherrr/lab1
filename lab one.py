ebab7ef1bbf623acd94a36ba0944b357eaac682e
Harmonwuzherrr <Harwat20@yahoo.com>
2014-10-10 15:01:24 -0500

    first

---
+++ lab one.py
@@ -0,0 +1,12 @@
+print "Welcome whats your name?"
+name=raw_input() 
+print "Hello " + name 
+print "What is your First Integer"
+firstinteger=raw_input()
+print "If you want to add then press one" 
+one=raw_input()
+print "What is your Second Integer"
+secondinteger=raw_input() 
+print "Your total is "
+total= int(firstinteger) + int(secondinteger)
+print total
